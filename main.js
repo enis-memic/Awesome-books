3bf2d47e69e9d8d972bc459bfccf798de4b60d6e
Enis <enis.mema@gmail.com>
2023-02-13 13:35:45 +0100

    Add javascript file with Add function

---
+++ main.js
@@ -0,0 +1,16 @@
+let bookCollection = [];
+
+const addBook = () => {
+  const titleInput = document.getElementById('title-input');
+  const authorInput = document.getElementById('author-input');
+
+  const title = titleInput.value;
+  const author = authorInput.value;
+
+  const newBook = { title, author };
+
+  bookCollection.push(newBook);
+
+  titleInput.value = '';
+  authorInput.value = '';
+};
